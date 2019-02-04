6c36bd6ce7a0f78a172d5829205af463e8424631
Angus McLeod <angus@mcleod.org.au>
2019-02-04 11:00:31 +1100

    Add extension

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Angus McLeod on 2/4/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
